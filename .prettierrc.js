73678fbdac33ad361b9ad96449d2d593c7f49d33
Ewan Morrison <ewanmorrrison@gmail.com>
2020-08-02 23:51:40 +0100

    Initial refresh commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 1,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "es5",
+	quoteProps: "consistent",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	endOfLine: "lf",
+    arrowParens: "always"
+};
